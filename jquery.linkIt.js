74817cdb6ec7789ea2836a7de49feefd7b52085f
Dan Giovanni <ddespley@hotmail.com>
2015-07-22 18:55:46 +0100

    0.2.0 release

--- jquery.linkIt.js
+++ jquery.linkIt.js
@@ -1,7 +1,38 @@
 /*
  * name: 	LinkIt
- * author: 	Brad Traversy
+ * author: 	Daniel Espley
  * version: 0.1.0
  * license: MIT
  */
  
+(function($)
+{
+	$.fn.linkIt = function(options)
+	{
+		// Default settings
+		var settings = $.extend(
+		{
+			href : null
+		}, 
+		options);
+
+		// Validate
+		if (settings.href == null)
+		{
+			console.log('You need an href option for linkIt to work.');
+			return this;
+		}
+
+		return this.each(function()
+		{
+			var span = $(this);
+			// Use text in <span> tag if none supplied in settings
+			if (settings.text == null)
+			{
+				settings.text = span.text(); 
+			}
+			span.wrap('<a href="' + settings.href +'" target="' + settings.target + '"></a>')
+			    .text(settings.text);
+		});
+	}
+} (jQuery) );
